f1eff1da3973ee7e2e601dbdd5b5fcea2e7b8b07
maiykill <70015530+maiykill@users.noreply.github.com>
2024-11-04 16:44:58 +0530

    upd: wezterm lf

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,19 +5,24 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -----------------------------------------------------------------
-
 -- I defined those below
+
 config.font_size = 16
 config.font = wezterm.font("FiraCode Nerd Font Ret", { weight = "Regular" })
 
--- For example, changing the color scheme:
+-- Theming
 -- config.color_scheme = "AdventureTime"
 config.color_scheme = "UnderTheSea"
 
 -- Maximised view since by default the window is small
-config.initial_rows = 34
-config.initial_cols = 136
-config.window_decorations = "NONE"
+config.initial_rows = 35
+config.initial_cols = 135
+
+-- resizing the font makes the widow small or bigger annoying!!
+config.window_decorations = "RESIZE"
+
+-- remove the annoying tab bar at the top
+config.enable_tab_bar = false
 
 -------------------------------------------------------------------
 
