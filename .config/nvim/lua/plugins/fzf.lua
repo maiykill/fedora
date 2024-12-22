1c84f500b34953b055aea001ae8d59fc3178f47c
maiykill <70015530+maiykill@users.noreply.github.com>
2024-12-23 00:26:16 +0530

    add: nvim/fzf

---
+++ .config/nvim/lua/plugins/fzf.lua
@@ -0,0 +1,20 @@
+return {
+  "ibhagwan/fzf-lua",
+  keys = {
+    {
+      "<leader>fh",
+      LazyVim.pick("files", { cwd = "~" }),
+      desc = "Find in Home",
+    },
+  },
+  opts = {
+    winopts = {
+      height = 0.85,
+      width = 0.95,
+      preview = {
+        horizontal = "right:50%",
+        layout = "horizontal",
+      },
+    },
+  },
+}
